3a4a83607752b56fdf5b846a870f46f12fb1341e
DaRk-plays <vedheshas@gmail.com>
2024-10-28 21:29:36 +0530

    extract text from pdf

--- pro1.py
+++ pro1.py
@@ -28,4 +28,12 @@ def preprocess_text(text):
 
     return cleaned_text
 
+# %%
+def extract_text_from_pdf(pdf_path):
+    text = ""
+    with pdfplumber.open(pdf_path) as pdf:
+        for page in pdf.pages:
+            text += page.extract_text()
+    return preprocess_text(text)
+# %%
 
