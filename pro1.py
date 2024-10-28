a3aa56623a79cb92c911604cb4082e122e6e015f
DaRk-plays <vedheshas@gmail.com>
2024-10-28 21:30:19 +0530

    process resume

--- pro1.py
+++ pro1.py
@@ -36,4 +36,13 @@ def extract_text_from_pdf(pdf_path):
             text += page.extract_text()
     return preprocess_text(text)
 # %%
+def process_resumes_from_nested_folders(main_folder):
+    resumes_data = {}
+    for subdir, _, files in os.walk(main_folder):
+        for file in files:
+            if file.endswith('.pdf'):
+                pdf_path = os.path.join(subdir, file)
+                extracted_text = extract_text_from_pdf(pdf_path)
+                resumes_data[file] = extracted_text
+    return resumes_data
 
