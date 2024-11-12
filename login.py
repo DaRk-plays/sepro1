ec5489729fa748a0d276601ae8bfdec6e36e2150
DaRk-plays <vedheshas@gmail.com>
2024-11-12 07:05:41 +0530

    creating database and tables

--- login.py
+++ login.py
@@ -10,4 +10,19 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+#%%
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(150), nullable=False)
 
+    def __init__(self, username, password):
+        self.username = username
+        self.password = generate_password_hash(password)
+
+# %%
+
+with app.app_context():
+    db.create_all()
+
+# %%
